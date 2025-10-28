4b81150058624f36cea705c5c03ce1f4b074ff26
dellannegabryel-commits <dellanne.gabryel@mail.uft.edu.br>
2025-10-28 14:13:50 -0300

    chore: refatoracao do auth-service

--- services/auth/views/partials/header.php
+++ services/auth/views/partials/header.php
@@ -5,33 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/public/js/auth.js"></script>
-    
-
-
-
-
+    <script src="/public/js/notifications.js"></script>
     <title><?php echo $title; ?> - VirtuaLib</title>
+    <link rel="stylesheet" href="/public/css/base.css">
+    <link rel="stylesheet" href="/public/css/navbar.css">
+    <link rel="stylesheet" href="/public/css/layout.css">
+    <link rel="stylesheet" href="/public/css/book-card.css">
+    <link rel="stylesheet" href="/public/css/stats.css">
+    <link rel="stylesheet" href="/public/css/profile.css">
+    <link rel="stylesheet" href="/public/css/notifications.css">
+    <link rel="stylesheet" href="/public/css/toast.css">
     
-    <style><?php
-        include __DIR__ . "../../public/css/base.css";
-        include __DIR__ . "../../public/css/navbar.css";
-        include __DIR__ . "../../public/css/layout.css";        
-        include __DIR__ . "../../public/css/profile.css";
-        include __DIR__ . "../../public/css/notifications.css";
-    ?>
-    </style>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
 
 <body>
     <?php require_once __DIR__ . '/navbar.php'; ?>
-    <script>
-        <?php 
-            include __DIR__ . '../../public/js/auth.js';
-            include __DIR__ . "../../public/js/navbar.js";
-            include __DIR__ . "../../public/js/bookmark.js";
-            include __DIR__ . "../../public/js/button.js";
-            include __DIR__ . "../../public/js/notifications.js";
-        ?>
-    </script>
+
+    <script src="/public/js/navbar.js"></script>
+    <script src="/public/js/bookmark.js"></script>
+
+    <script src="/public/js/toast.js"></script>
+    <script src="/public/js/borrow.js"></script>
     
